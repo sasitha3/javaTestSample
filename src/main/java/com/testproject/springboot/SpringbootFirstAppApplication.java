590c1b42b6b1896c0221256b787536e58665bf86
Sasitha Premadasa <sasitha.premadasa@mcmedisoft.com>
2023-01-26 23:34:46 +0530

    Creating Sample API, Initial stage

--- src/main/java/com/testproject/springboot/SpringbootFirstAppApplication.java
+++ src/main/java/com/testproject/springboot/SpringbootFirstAppApplication.java
@@ -8,22 +8,7 @@ import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class SpringbootFirstAppApplication {
-
-	private static LinkedHashMap<String, Integer> vote = new LinkedHashMap<>();
-
-	static void vote(String contestant){
-		vote.merge(contestant, 1, Integer::sum);
-	}
-
-	public static LinkedHashMap getVotes() { return vote; }
 	public static void main(String[] args) {
-
-
-		vote("TV 1");
-		vote("TV 1");
-		vote("TV 1");
-		vote("TV 2");
-		System.out.println(getVotes());
 
 		SpringApplication.run(SpringbootFirstAppApplication.class, args);
 	}
