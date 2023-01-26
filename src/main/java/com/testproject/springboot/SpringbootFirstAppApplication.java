b5d0ef3a3afbf3640dbf71e414b30d1cd54347af
Sasitha Premadasa <sasitha.premadasa@mcmedisoft.com>
2023-01-26 23:44:44 +0530

    created API

--- src/main/java/com/testproject/springboot/SpringbootFirstAppApplication.java
+++ src/main/java/com/testproject/springboot/SpringbootFirstAppApplication.java
@@ -1,12 +1,15 @@
 package com.testproject.springboot;
 
+import com.testproject.controller.EmployeeController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = EmployeeController.class)
 public class SpringbootFirstAppApplication {
 	public static void main(String[] args) {
 
